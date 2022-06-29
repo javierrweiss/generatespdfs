44bdbd6aa956feb1c8dd3d099e09085fe3595be6
José Javier Blanco Rivero <javierweiss@gmail.com>
2022-06-29 20:53:24 +0000

    initial commit

---
+++ generatespdfs/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to javierweiss/generatespdfs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ generatespdfs/test/javierweiss/generatespdfs_test.clj
@@ -0,0 +1,7 @@
+(ns javierweiss.generatespdfs-test
+  (:require [clojure.test :refer :all]
+            [javierweiss.generatespdfs :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
